f164e500703ac3a19d927cbe46315965c545decc
Grynko Andrey <115623463+benteylor@users.noreply.github.com>
2023-12-18 20:30:17 +0200

    test

---
+++ src/test/TestForGitHub.java
@@ -0,0 +1,13 @@
+public class TestForGitHub {
+        public static void main(String[] args) {
+            // Стороны треугольника
+            double sideA = 5;
+            double sideB = 5;
+            // Расчёт
+            double S = sideA * sideB/2;
+            // Результат
+            System.out.printf("Площадь прямоугольного треугольника: "  +S);
+        }
+    }
+
+
