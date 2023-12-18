52b191377cf82917aab27d5f36e172d5f3b2770d
Grynko Andrey <115623463+benteylor@users.noreply.github.com>
2023-12-18 19:47:29 +0200

    Task6RectangularTriangle

---
+++ src/main/java/Task6RectangularTriangle.java
@@ -0,0 +1,11 @@
+public class Task6RectangularTriangle {
+    public static void main(String[] args) {
+        // Стороны треугольника
+        double sideA = 5;
+        double sideB = 5;
+        // Расчёт
+        double S = sideA * sideB/2;
+        // Результат
+        System.out.printf("Площадь прямоугольного треугольника: "  +S);
+    }
+}
